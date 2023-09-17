5b23904ad7037077bf242d6baf84fea8f82d09ae
Douglas Candido <douglascandido@workmail.com>
2023-09-17 14:05:57 -0300

    Add user controller initial boilerplate config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Post } from '@nestjs/common';
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController implements IUser {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async createUser(@Body() dto: CreateUserDTO): Promise<User> {
+    return await this.userService.createUser(dto);
+  }
+
+  @Patch()
+  async updateUser(@Body() dto: UpdateUserDTO): Promise<User> {
+    return await this.userService.updateUser(dto);
+  }
+
+  @Delete()
+  async deleteUser() {}
+}
