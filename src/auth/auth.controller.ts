808381ef007b02f038ec441b325b17630c480d40
Douglas Candido <douglascandido@workmail.com>
2023-09-17 15:05:09 -0300

    Add initial auth controller boilerplate config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { IAuth } from './interfaces/auth.interface';
+import { AuthService } from './auth.service';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController implements IAuth {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async loginUser(
+    @Body() dto: LoginUserDTO,
+  ): Promise<{ access_token: string }> {
+    return await this.authService.loginUser(dto);
+  }
+}
