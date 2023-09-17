1349188c569aaadc2373828701bdaeb663b747ba
Douglas Candido <douglascandido@workmail.com>
2023-09-17 18:52:02 -0300

    Add update post dto

---
+++ src/posts/dto/update-post.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePostDTO } from './create-post.dto';
+
+export class UpdatePostDTO extends PartialType(CreatePostDTO) {
+  readonly content?: string;
+}
