78f5ffe1b26b4e90e42c7af4d5cddbe5aa39be66
Douglas Candido <douglascandido@workmail.com>
2023-09-17 18:06:23 -0300

    Add posts folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, PostsModule],
   controllers: [],
   providers: [],
 })
