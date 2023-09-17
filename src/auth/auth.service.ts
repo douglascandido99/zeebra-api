e7bd9c89f14ecbf808be857fc1b0920f5100ca76
Douglas Candido <douglascandido@workmail.com>
2023-09-17 15:05:57 -0300

    Add initial auth service boilerplate config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { IAuth } from './interfaces/auth.interface';
+import { LoginUserDTO } from './dto/login-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService implements IAuth {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
+  loginUser(dto: LoginUserDTO): Promise<{ access_token: string }> {}
+  signToken(
+    userId: number,
+    username: string,
+  ): Promise<{ access_token: string }> {}
+}
