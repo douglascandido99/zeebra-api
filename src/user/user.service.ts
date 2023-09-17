ccb8a7ec8b55a5a94c4cbeb4081508d762525542
Douglas Candido <douglascandido@workmail.com>
2023-09-17 14:06:18 -0300

    Add initial user service boilerplate config

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService implements IUser {
+  constructor(private readonly prisma: PrismaService) {}
+  async createUser(dto: CreateUserDTO): Promise<User> {}
+  async updateUser(dto: UpdateUserDTO): Promise<User> {}
+  async deleteUser() {}
+}
