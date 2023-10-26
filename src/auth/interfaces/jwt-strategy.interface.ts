11457a981431b3fb1637c6ec97791eb8fe9b9c36
Douglas Candido <douglascandido@workmail.com>
2023-10-26 15:05:45 -0300

    chore: remove unused files and folders

--- src/auth/interfaces/jwt-strategy.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IJwtStrategy {
-  validate(payload: { sub: number; username: string });
-}
--- src/user/interfaces/create-user.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ICreateUser {
-  readonly name: string;
-  readonly email: string;
-  readonly username: string;
-  readonly password: string;
-}
